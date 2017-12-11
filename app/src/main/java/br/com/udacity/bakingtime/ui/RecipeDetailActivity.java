f2d9a41ab470fec0a83fadc49db95dd8ee5ed657
andre.oliveira <andre.oliveira@funcate.org.br>
2017-12-11 14:08:58 -0200

    fixes: Fixing the layout. The Master/flow layout has been passed to the details activity.

--- app/src/main/java/br/com/udacity/bakingtime/ui/RecipeDetailActivity.java
+++ app/src/main/java/br/com/udacity/bakingtime/ui/RecipeDetailActivity.java
@@ -2,23 +2,35 @@ package br.com.udacity.bakingtime.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import br.com.udacity.bakingtime.R;
+import br.com.udacity.bakingtime.dummy.DummyContent;
+import br.com.udacity.bakingtime.adapters.SimpleItemRecyclerViewAdapter;
 
 /**
- * An activity representing a single Recipe detail screen. This
- * activity is only used on narrow width devices. On tablet-size devices,
- * item details are presented side-by-side with a list of items
- * in a {@link RecipeListActivity}.
+ * An activity representing a list of recipe steps. This activity
+ * has different presentations for handset and tablet-size devices.
+ * On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
  */
 public class RecipeDetailActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    /**
+     * Recycler view used to display a list of recipe steps
+     */
+    private RecyclerView mStepsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        if (findViewById(R.id.recipe_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        mStepsRecyclerView = findViewById(R.id.steps_list);
+        assert mStepsRecyclerView != null;
+        setupRecyclerView(mStepsRecyclerView);
 
         // savedInstanceState is non-null when there is fragment state
         // saved from previous configurations of this activity
@@ -70,5 +94,9 @@ public class RecipeDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(mTwoPane, DummyContent.ITEMS, this));
     }
 }
--- app/src/main/java/br/com/udacity/bakingtime/ui/RecipeListActivity.java
+++ app/src/main/java/br/com/udacity/bakingtime/ui/RecipeListActivity.java
@@ -20,25 +20,24 @@ import android.widget.TextView;
 
 
 import br.com.udacity.bakingtime.R;
+import br.com.udacity.bakingtime.adapters.SimpleItemRecyclerViewAdapter;
 import br.com.udacity.bakingtime.dummy.DummyContent;
 
 import java.util.List;
 
 /**
- * An activity representing a list of Recipes. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link RecipeDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
+ * An activity representing a list of Recipes. The activity presents a list of items,
+ * which when touched, lead to a {@link RecipeDetailActivity} representing item details.
+ *
+ * On tablet-size devices, item details are presented side-by-side with a list of items
+ * in a {@link RecipeDetailActivity}.
  */
 public class RecipeListActivity extends AppCompatActivity {
 
     /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
+     * Recycler view used to display a list of Recipes.
      */
-    private boolean mTwoPane;
+    private RecyclerView mRecipesRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,25 +48,10 @@ public class RecipeListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        if (findViewById(R.id.recipe_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
+        mRecipesRecyclerView = findViewById(R.id.recipe_list);
+        assert mRecipesRecyclerView != null;
+        setupRecyclerView(mRecipesRecyclerView);
 
-        RecyclerView recyclerView = findViewById(R.id.recipe_list);
-        assert recyclerView != null;
-
-        //if (mTwoPane) {
-        //    recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-        //} else {
-        //    recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-        //}
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-
-        setupRecyclerView(recyclerView);
         handleIntent();
     }
 
@@ -86,76 +70,6 @@ public class RecipeListActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
-    }
-
-    public static class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final RecipeListActivity mParentActivity;
-        private final List<DummyContent.DummyItem> mValues;
-        private final boolean mTwoPane;
-        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
-                if (mTwoPane) {
-
-                    Bundle arguments = new Bundle();
-                    arguments.putString(RecipeDetailFragment.ARG_ITEM_ID, item.id);
-                    RecipeDetailFragment fragment = new RecipeDetailFragment();
-                    fragment.setArguments(arguments);
-                    mParentActivity.getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.recipe_detail_container, fragment)
-                            .commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, RecipeDetailActivity.class);
-                    intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID, item.id);
-
-                    context.startActivity(intent);
-                }
-            }
-        };
-
-        SimpleItemRecyclerViewAdapter(RecipeListActivity parent,
-                                      List<DummyContent.DummyItem> items,
-                                      boolean twoPane) {
-            mValues = items;
-            mParentActivity = parent;
-            mTwoPane = twoPane;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.recipe_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
-
-            holder.itemView.setTag(mValues.get(position));
-            holder.itemView.setOnClickListener(mOnClickListener);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            final TextView mIdView;
-            final TextView mContentView;
-
-            ViewHolder(View view) {
-                super(view);
-                mIdView = view.findViewById(R.id.id_text);
-                mContentView = view.findViewById(R.id.content);
-            }
-        }
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(false, DummyContent.ITEMS, this));
     }
 }
